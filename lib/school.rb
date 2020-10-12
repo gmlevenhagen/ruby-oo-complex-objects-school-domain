52c49f92824050c0c52f2cd7bb9a4b9872e622a6
gmlevenhagen <garettlevenhagen@gmail.com>
2020-10-12 13:23:18 -0500

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,44 @@
-# code here!
+class School
+
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+
+    def roster
+        @roster
+    end
+
+    def add_student(name, grade)
+       if roster[grade] ||= []
+        roster[grade] << name
+    end
+
+    def grade(grade)
+        roster.detect do |key, value|
+            if key == grade
+                return value
+            end
+        end
+    end
+    
+
+    def sort
+        roster.each do |key, value|
+            value.sort!
+        end
+        roster.sort.to_h
+    end
+
+   
+end
+
+
+
+
+
+
+
+
+    
+end
